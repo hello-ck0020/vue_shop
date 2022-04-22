5e5441133e20b45528772c3e50c0ac1c432771b5
hellock0020 <13152443517@163.com>
2022-04-22 15:06:10 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
 Vue.config.productionTip = false
-
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 new Vue({
